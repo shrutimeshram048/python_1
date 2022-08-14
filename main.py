d89b2aa4958dbab8d16ff3a99696369f5eeb8994
shrutimeshram048 <98177436+shrutimeshram048@users.noreply.github.com>
2022-08-14 15:13:16 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,42 +1,46 @@
-# import colorgram
-#
-# rgb_colors = []
-# colors = colorgram.extract("image.jpg", 21)
-# for color in colors:
-#     r = color.rgb.r
-#     g = color.rgb.g
-#     b = color.rgb.b
-#     new_color = (r, g, b)
-#     rgb_colors.append(new_color)
-#
-# print(rgb_colors)
-import turtle
-from turtle import Turtle, Screen
-import random
-turtle.colormode(255)
-tim = Turtle()
-tim.speed("fastest")
-tim.penup()
-tim.hideturtle()
-color_list = [(236, 35, 108), (221,   232, 237), (145, 28, 64), (239, 75, 35), (6, 148, 93), (232, 238, 234),
-              (231, 168, 40), (184, 158, 46), (44, 191, 233), (27, 127, 195), (126, 193, 74), (253, 223, 0),
-              (85, 28, 93), (173, 36, 97), (246, 219, 44), (44, 172, 112), (215, 130, 165), (215, 56, 27),
-              (235, 164, 191)]
-
-tim.setheading(225)
-tim.forward(300)
-tim.setheading(0)
-number_of_dots = 100
-for dot_count in range(1, number_of_dots + 1):
-    tim.dot(20, random.choice(color_list))
-    tim.forward(50)
-    if dot_count % 10 == 0:
-        tim.setheading(90)
-        tim.forward(50)
-        tim.setheading(180)
-        tim.forward(500)
-        tim.setheading(0)
-
+from turtle import Screen, Turtle
+from paddle import Paddle
+from ball import Ball
+from scoreboard import ScoreBoard
+import time
+turtle = Turtle()
 screen = Screen()
-screen.exitonclick()
+ball = Ball()
+score_board = ScoreBoard()
+screen.tracer(0)
+screen.setup(width=800, height=600)
+screen.title("PONG")
+screen.bgcolor("black")
 
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+
+screen.listen()
+screen.onkey(r_paddle.up, "Up")
+screen.onkey(r_paddle.down, "Down")
+screen.onkey(l_paddle.up, "w")
+screen.onkey(l_paddle.down, "s")
+
+game_is_on = True
+while game_is_on:
+    time.sleep(ball.ball_speed)
+    screen.update()
+    ball.refresh_ball()
+
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+
+    # R paddle misses
+    if ball.xcor() > 350:
+        ball.reset_position()
+        score_board.update_l_score()
+
+    # L paddle misses
+    if ball.xcor() < -350:
+        ball.reset_position()
+        score_board.update_r_score()
+
+screen.exitonclick()
--- paddle.py
+++ paddle.py
@@ -1,10 +1,19 @@
 from turtle import Turtle
 
 
-class Paddle:
-    paddle = Turtle(shape="square")
-    paddle.color("white")
-    paddle.penup()
-    paddle.shapesize(stretch_wid=5, stretch_len=1)
+class Paddle(Turtle):
+    def __init__(self, position):
+        super().__init__()
+        self.shape("square")
+        self.shapesize(stretch_len=1, stretch_wid=5)
+        self.color("white")
+        self.penup()
+        self.goto(position)
 
-    def co(self, ):
+    def up(self):
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), new_y)
+
+    def down(self):
+        new_y = self.ycor() - 20
+        self.goto(self.xcor(), new_y)
